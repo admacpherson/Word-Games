732230e2248722204378d2705dee6c5303f966ef
admacpherson <102562791+admacpherson@users.noreply.github.com>
2025-05-21 07:39:33 -0700

    Gameboard setup and dictionary API word checker

--- letterboxed.js
+++ letterboxed.js
@@ -1,2 +1,13 @@
-console.log("Hello world!");
+// Define game board
+var top = ['W', 'M', 'Y'];
+var left = ['S', 'L', 'R'];
+var bottom = ['H', 'N', 'D'];
+var right = ['T', 'O', 'I'];
+const square = [top, left, bottom, right];
+const square_order = ["Top", "Left", "Bottom", "Right"];
 
+//Determine if a word is valid
+async function isValidWord(word) {
+  const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`);
+  return response.ok; // true if found
+}
