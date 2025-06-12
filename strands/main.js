25aa0487ccedfb1bab3faf3cf920a5e076c5464c
admacpherson <102562791+admacpherson@users.noreply.github.com>
2025-06-12 15:43:37 -0700

    More grid setup

--- strands/main.js
+++ strands/main.js
@@ -1,9 +1,13 @@
-// Puzzle data
-const rows = 8;
-const cols = 6;
+/**************
+**CREATE GRID**
+***************/
+
+// Grid size
+const numRows = 8;
+const numCols = 6;
 
 // Example 8x6 letter grid - scrambled letters including words: APPLE, GRAPE, PEAR, MANGO
-  const letters = [
+const letters = [
     'A','P','P','L','E','X',
     'X','G','R','A','P','E',
     'X','X','P','E','A','R',
@@ -17,16 +21,41 @@ const cols = 6;
 // Valid words in this puzzle
 const validWords = ['APPLE', 'GRAPE', 'PEAR', 'MANGO'];
 
+// Grid div element
 const grid = document.getElementById("grid");
 
-function createCell(letter, index) {
+// Create a div element with class "cell" and an index
+function createCell(letter, row, col) {
     const cell = document.createElement("div");
     cell.className = "cell";
     cell.textContent = letter;
-    cell.dataset.index = index;
+    cell.dataset.row = row;
+    cell.dataset.col = col;
     return cell
 }
 
-letters.forEach((letter, i) => {
-    grid.appendChild(createCell(letter, i));
-})
+// Create a cell for each letter
+function createGrid() {
+    for (let row = 0; row < numRows; row++) {
+        for (let col = 0; col < numCols; col++) {
+            const letterIndex = row * col + col;
+            const letter = letters[letterIndex];
+            grid.appendChild(createCell(letter, row, col));
+        }
+    }
+}
+
+createGrid();
+
+/*******************
+**HELPER FUNCTIONS**
+********************/
+
+// Helper function to get index
+function getCellByIndex(i) {
+    return grid.querySelector(`".cell[data-row="${row}"][data-col="${col}"]"`);
+}
+
+/*************
+**USER INPUT**
+**************/
