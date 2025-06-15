03131628a9f6f4950c2ae15d94868ef001ad280e
admacpherson <102562791+admacpherson@users.noreply.github.com>
2025-06-14 22:09:31 -0700

    Basic logic

--- strands/main.js
+++ strands/main.js
@@ -38,14 +38,25 @@ function createCell(letter, row, col) {
 function createGrid() {
     for (let row = 0; row < numRows; row++) {
         for (let col = 0; col < numCols; col++) {
-            const letterIndex = row * col + col;
+            const letterIndex = row * numCols + col;
             const letter = letters[letterIndex];
             grid.appendChild(createCell(letter, row, col));
         }
     }
 }
 
+// Keep track of whether user is dragging/swiping
+let isDragging = false;
+// Keep track of whether user has been dragging/swiping
+let hasDragged = false;
+// Store letters that have been selected
+let selectedCells = [];
+// Keep track of current word
+let selectedWord = "";
+
 createGrid();
+resetSelection();
+updateBannerText("Test");
 
 /*******************
 **HELPER FUNCTIONS**
@@ -56,6 +67,98 @@ function getCellByIndex(i) {
     return grid.querySelector(`".cell[data-row="${row}"][data-col="${col}"]"`);
 }
 
+// Mark a cell/letter as selected
+function selectCell(cell) {
+    //Ensure it is not already selected
+    if (selectedCells.includes(cell)) {
+        return
+    }
+    
+    cell.classList.add("selected");
+    selectedCells.push(cell);
+}
+
+// Reset current selection of cells/letters
+function resetSelection() {
+    selectedCells.forEach(cell => cell.classList.remove("selected"));
+    selectedCells = [];
+    selectedWord = "";
+}
+
+// Update the text in the top banner
+function updateBannerText(message) {
+    document.getElementById("message").innerText = message;
+}
+
+function handleGuess() {
+    //Join all selected cells into an overall word
+    selectedWord = selectedCells.map(cell => cell.textContent).join('');
+    //Check if correct
+    if (validWords.includes(selectedWord)) {
+        updateBannerText("Correct");
+    } else {
+        updateBannerText("Incorrect");
+    }
+    resetSelection();
+}
+
 /*************
 **USER INPUT**
 **************/
+
+// Pointer down event listener
+grid.addEventListener("pointerdown", (e) => {
+    //Ignore clicks outside of the grid
+   if (!e.target.classList.contains("cell")) {
+       return;
+   }
+    // Note that the user is holding down
+    isDragging = true;
+    // Reset drag status
+    hasDragged = false;
+});
+
+// Pointer movement event listener
+grid.addEventListener("pointermove", (e) => {
+    // Ignore if the user isn't holding down or on the grid
+    if (!isDragging || !e.target.classList.contains("cell")) {
+        return;
+    }
+    
+    if(!selectedCells.includes(e.target) && isDragging) {
+        //Select the cell the user moves over
+        selectCell(e.target);
+        // Note the user has been dragging
+        hasDragged = true;
+    }
+    
+});
+
+// Pointer up event listener
+grid.addEventListener("pointerup", () => {
+    isDragging = false;
+    // Only finalize guess if they are done dragging (ignore clicks)
+    if (hasDragged) {
+        handleGuess();
+    }
+    
+    // Indicate that the user is no longer holding down
+    hasDragged = false;
+})
+
+grid.addEventListener("click", (e) => {
+    //Ignore clicks outside of the grid
+   if (!e.target.classList.contains("cell")) {
+       return;
+   }
+    
+    // Check if the selected element is the same as the last cell selected (i.e. double tapped)
+    const isLastSelected = selectedCells[selectedCells.length - 1] === e.target;
+    
+    // If already selected, confirm word, else add to word
+    if (isLastSelected && selectedCells.includes(e.target)) {
+        handleGuess();
+    } else {
+        selectCell(e.target);
+    }
+})
